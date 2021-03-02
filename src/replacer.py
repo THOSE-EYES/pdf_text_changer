81551f3d61e41c417eec4355133da6ecf325fced
THOSE-EYES <illya.shvarov@gmail.com>
2021-03-02 19:59:57 +0100

    Update program comments

--- src/replacer.py
+++ src/replacer.py
@@ -4,7 +4,17 @@ import json
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PyPDF2.generic import DecodedStreamObject, NameObject, NullObject
 
+'''
+Class Replacer is created in order to replace data 
+inside of the PDF. The data is replaced using JSON
+object, which has the following structure : 
+{"data_to_replace" : "replacer"}
+'''
 class Replacer :
+	'''
+	Constructor. It takes 2 arguments : an input PDF
+	file and a JSON object of data to replace
+	'''
 	def __init__(self, input, replacements):
 		# Open PDF streams
 		self._input = input
@@ -12,6 +22,9 @@ class Replacer :
 		self._outputPDF = PdfFileWriter()
 		self._replacements = replacements
 
+	'''
+	Replace text data inside of the content object
+	'''
 	def __replaceText(self, content):
 		lines = content.splitlines()
 		result = str()
@@ -23,6 +36,9 @@ class Replacer :
 
 		return result
 
+	'''
+	Decode the content and process the data
+	'''
 	def __processContent(self, content):
 		data = content.getData()
 
@@ -37,6 +53,10 @@ class Replacer :
 
 		return decodedContent
 
+	'''
+	Processing every page of the document and
+	replacing the desired text
+	'''
 	def __processFile(self):
 		# Iterate over the pages
 		for number in range(0, self._inputPDF.getNumPages()):
@@ -58,17 +78,26 @@ class Replacer :
 			# Save the page
 			self._outputPDF.addPage(page)
 
+	'''
+	Public method which calls the internal ones in
+	order to replace the desired text. Then it 
+	saves the file.
+	'''
 	def process(self):
 		self.__processFile()
 
+		# Check if the output file is not empty
 		if self._outputPDF.getNumPages() != 0 :
+			# Save the document
 			with open(self._input + ".result.pdf", 'wb') as out_file:
 				print ("Saving...")
 				self._outputPDF.write(out_file)
 		else :
 			print("Nothing to write. Exiting...")
 
-
+'''
+Get command line parameters
+'''
 def get_arguments():
 	# Get data from the user
 	parser = argparse.ArgumentParser()
@@ -82,6 +111,10 @@ def get_arguments():
 
 	return input_file, replacements
 
+'''
+Main function which is called when the
+module is running as a standalone module
+'''
 def main():
 	input_file, replacements = get_arguments()
 
@@ -89,5 +122,8 @@ def main():
 	replacer = Replacer(input_file, replacements)
 	out = replacer.process()
 
+'''
+Check if the module is standalone
+'''
 if __name__ == "__main__":
 	main()
